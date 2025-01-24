d027d1389bdea61d04900fdf4ff676a417ea3b13
josephflo <josephflo.dev@gmail.com>
2025-01-23 23:57:47 -0300

    feat/hero

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,13 @@
+import BookList from "@/components/BookList";
+import BookOverview from "@/components/BookOverview";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <main>
-    <Button>Click me</Button>
-   </main>
+   <>
+    <BookOverview />
+    <BookList />
+   </>
   );
 }
